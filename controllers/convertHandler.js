e68e43e4eef0f3df65ad5f0a5e92a2d0545427cc
Samuel Christopher <Ebusameric@gmail.com>
2019-06-10 18:34:00 +0100

    feat: complete the data handler methods that manipulates the data

--- controllers/convertHandler.js
+++ controllers/convertHandler.js
@@ -5,48 +5,60 @@
 *       
 *       
 */
+const units = {
+      'gal':'l', 'lbs':'kg', 'mi':'km',
+      'l':'gal', 'kg':'lbs', 'km':'mi'
+    };
 
 function ConvertHandler() {
-  
   this.getNum = function(input) {
-    var result;
-    
-    return result;
+    let num = input.match(/[\d./]+/);
+    if(!num) return 'No Numerical Input';
+    if(/(.*\/.*\/.*)/.test(num[0])) return 'Invalid Input';
+    return num[0];
   };
   
-  this.getUnit = function(input) {
-    var result;
-    
-    return result;
+  this.getUnit = function(input) {    
+    let unit = input.match(/[a-z]+$/i);
+    if(!unit) return 'No Unit';
+    unit = unit[0].toLowerCase();
+    if(!units[unit]) return 'Unknown Unit Input';
+    return unit;
   };
   
   this.getReturnUnit = function(initUnit) {
-    var result;
-    
-    return result;
+    return units[initUnit];
   };
 
   this.spellOutUnit = function(unit) {
-    var result;
-    
-    return result;
+    return {
+      'gal':'gallons', 'l':'litres', 'lbs':'pounds',
+      'kg':'kilograms', 'mi':'miles', 'km':'kilometers',
+    }[unit];
   };
   
   this.convert = function(initNum, initUnit) {
     const galToL = 3.78541;
     const lbsToKg = 0.453592;
     const miToKm = 1.60934;
-    var result;
-    
-    return result;
+    initNum = eval(initNum);
+    let resulting = {
+      'gal': initNum*galToL, 'lbs':initNum*lbsToKg, 'mi': initNum*miToKm,
+      'l': initNum/galToL, 'kg': initNum/lbsToKg, 'km': initNum/miToKm 
+    }
+    return Math.round(resulting[initUnit]);
   };
   
   this.getString = function(initNum, initUnit, returnNum, returnUnit) {
-    var result;
-    
-    return result;
+    let oldUnit = this.spellOutUnit(initUnit);
+    let newUnit = this.spellOutUnit(returnUnit);
+    return `${initNum} ${oldUnit} converts to ${returnNum+.00002} ${newUnit}`;
   };
-  
+ 
+  // this.checkInput = function(input) {
+  //   if(!input.trim()) return 1;
+  //   if(input.match(/\/{2,}?\.{2,}?/g)) return 'invalid number';
+  // } 
 }
 
 module.exports = ConvertHandler;
