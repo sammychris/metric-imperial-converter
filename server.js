834bc31a236a032b5959080661e78728d94739d3
Samuel Christopher <Ebusameric@gmail.com>
2019-06-11 03:33:25 +0100

    feat: add helmet for security and also config it for use

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ var express     = require('express');
 var bodyParser  = require('body-parser');
 var expect      = require('chai').expect;
 var cors        = require('cors');
+var helmet      = require('helmet');
 
 require('dotenv').config(); // loading this environmental variables...
 
@@ -19,8 +20,19 @@ app.use(cors({origin: '*'})); //For FCC testing purposes only
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(helmet({
+  frameguard: {              // configure
+    action: 'deny'
+  },
+  contentSecurityPolicy: {   // enable and configure
+   directives: {
+     defaultSrc: ["'self'"],
+     styleSrc: ['style.com'],
+   }
+  },
+ dnsPrefetchControl: false   // disable
+}))
 
-//Index page (static HTML)
 app.route('/')
   .get(function (req, res) {
     res.sendFile(process.cwd() + '/views/index.html');
@@ -38,7 +50,7 @@ app.use(function(req, res, next) {
     .type('text')
     .send('Not Found');
 });
-console.log(process.env.PORT)
+
 //Start our server and tests!
 app.listen(process.env.PORT || 3000, function () {
   console.log("Listening on port " + process.env.PORT);
